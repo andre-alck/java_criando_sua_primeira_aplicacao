6edc050af800460c0b54175ed74222280dade03d
andre-alck <andrealck1@gmail.com>
2023-04-16 23:21:57 -0300

    feat: add 02

--- primeiroprojeto/src/Main.java
+++ primeiroprojeto/src/Main.java
@@ -2,6 +2,19 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello, world!");
+        System.out.println("Screen Match");
+
+        System.out.println("Filme: \"Top Gun: Maverick\"");
+
+        String sinopse = """
+                Filme de aventura
+                2022
+                """;
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        double media = (9.8 + 6.3) / 2;
+        int classificacao = (int) media / 2;
+        System.out.println(classificacao);
     }
 }
